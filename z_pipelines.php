22b325090edf81bfcdda7b8b1cf17371a9f4bf22
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-06-29 10:24:03 +0000

    ?page=toto peut etre definie uniquement par un squelette de coeur de page contenu/page-toto.html une page spip_pass pour changer sont mot de passe dans un coeur de page
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_/zesty@29462 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- z_pipelines.php
+++ z_pipelines.php
@@ -14,29 +14,16 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 
 function Z_styliser($flux){
 
-	/*
-	// les pages non implementees mais qui ont un contenu connu, passent par page.html
-	@define('_SPIP_PAGE','page');
-	if (isset($_GET[_SPIP_PAGE])
-	 AND $page = $_GET[_SPIP_PAGE]
-	 AND !find_in_path("$page.html")
-	 AND find_in_path("contenu/page-$page.html")) {
-		$_GET['presentation'] = $page;
-		$_GET[_SPIP_PAGE] = 'page';
+	$squelette = $flux['data'];
+	if (!$squelette // non trouve !
+		AND $fond = $flux['args']['fond']
+		AND $ext = $flux['args']['ext']
+	  AND $flux['args']['contexte'][_SPIP_PAGE] == $fond) {
+		$base = "contenu/page-".$fond.".".$ext;
+		if ($base = find_in_path($base)){
+			$flux['data'] = substr(find_in_path("page.$ext"), 0, - strlen(".$ext"));
+		}
 	}
-
-
-	// pipeline styliser
-	$squelette = pipeline('styliser', array(
-		'args' => array(
-			'id_rubrique' => $id_rubrique,
-			'ext' => $ext,
-			'fond' => $fond,
-			'lang' => $lang,
-			'connect' => $connect
-		),
-		'data' => $squelette,
-	));*/
 
 	return $flux;
 

d069b582fda15bb4a3a63b00e4901596bc0e18e7
cedric@yterium.com <cedric@yterium.com@ac52e18a-acf5-0310-9fe8-c4428f23b10a>
2009-07-18 11:33:30 +0000

    meilleure compatibilite avec les framework css qui resettent tout a zero et font du padding sur les elements de contenu et non sur les colonnes : - la hierarchie est dans un <p> - #INTRODUCTION est toujours paragraphee, car elle restait derogatoire par rapport aux autres champs avec des fois un <p> des fois non - une classe plan sur les ul du plan
    Integration d'un spip_styles minimal
    
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_/zpip@29954 ac52e18a-acf5-0310-9fe8-c4428f23b10a

--- z_pipelines.php
+++ z_pipelines.php
@@ -29,4 +29,63 @@ function Z_styliser($flux){
 
 }
 
+
+//
+// fonction standard de calcul de la balise #INTRODUCTION
+// mais retourne toujours dans un <p> comme propre
+//
+// http://doc.spip.org/@filtre_introduction_dist
+function filtre_introduction($descriptif, $texte, $longueur, $connect) {
+	// Si un descriptif est envoye, on l'utilise directement
+	if (strlen($descriptif))
+		return propre($descriptif,$connect);
+
+	// Prendre un extrait dans la bonne langue
+	$texte = extraire_multi($texte);
+
+	// De preference ce qui est marque <intro>...</intro>
+	$intro = '';
+	$texte = preg_replace(",(</?)intro>,i", "\\1intro>", $texte); // minuscules
+	while ($fin = strpos($texte, "</intro>")) {
+		$zone = substr($texte, 0, $fin);
+		$texte = substr($texte, $fin + strlen("</intro>"));
+		if ($deb = strpos($zone, "<intro>") OR substr($zone, 0, 7) == "<intro>")
+			$zone = substr($zone, $deb + 7);
+		$intro .= $zone;
+	}
+	$texte = $intro ? $intro : $texte;
+
+	// On ne *PEUT* pas couper simplement ici car c'est du texte brut, qui inclus raccourcis et modeles
+	// un simple <articlexx> peut etre ensuite transforme en 1000 lignes ...
+	// par ailleurs le nettoyage des raccourcis ne tient pas compte des surcharges
+	// et enrichissement de propre
+	// couper doit se faire apres propre
+	//$texte = nettoyer_raccourcis_typo($intro ? $intro : $texte, $connect);
+
+	// ne pas tenir compte des notes ;
+	// bug introduit en http://trac.rezo.net/trac/spip/changeset/12025
+	$mem = array($GLOBALS['les_notes'], $GLOBALS['compt_note'], $GLOBALS['marqueur_notes'], $GLOBALS['notes_vues']);
+	// memoriser l'etat de la pile unique
+	$mem_unique = unique('','_spip_raz_');
+
+
+	$texte = propre($texte,$connect);
+
+
+	// restituer les notes comme elles etaient avant d'appeler propre()
+	list($GLOBALS['les_notes'], $GLOBALS['compt_note'], $GLOBALS['marqueur_notes'], $GLOBALS['notes_vues']) = $mem;
+	// restituer l'etat de la pile unique
+	unique($mem_unique,'_spip_set_');
+
+
+	@define('_INTRODUCTION_SUITE', '&nbsp;(...)');
+	$texte = couper($texte, $longueur, _INTRODUCTION_SUITE);
+
+	// Fermer les paragraphes ; mais ne pas en creer si un seul
+	$texte = paragrapher($texte, $GLOBALS['toujours_paragrapher']);
+
+
+	return $texte;
+}
+
 ?>
